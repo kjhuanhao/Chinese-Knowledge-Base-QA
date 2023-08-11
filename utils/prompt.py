9334da4bd47edc49a6e1ac200fa1b5288fc1054a
laijiahao <2077780946@qq.com>
2023-08-11 14:58:56 +0800

    微调代码

--- utils/prompt.py
+++ utils/prompt.py
@@ -30,7 +30,7 @@ class PromptTool:
         header = f""" 你是一个文档查询助手，你的任务是根据一段参考文本，回答我的问题，如果我的问题和参考文本无关，请回复：对不起，知识库中无此相关答案"""
 
         prompt = header + "\n参考文本:\n" + f"'''{reference_documents}'''" + f"\n我的问题是: {question}"
-        print(prompt)
+
         return {
             "prompt": prompt,
             "documents": reference_documents
